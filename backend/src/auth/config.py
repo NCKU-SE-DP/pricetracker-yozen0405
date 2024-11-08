eb9420bd31c27b03b771aa66957d59df37584c8a
yozen0405 <yozen0405@gmail.com>
2024-11-08 09:14:35 +0800

    fix(config): revert the changes

--- backend/src/auth/config.py
+++ backend/src/auth/config.py
@@ -1,9 +1,8 @@
 from pydantic_settings import BaseSettings
-import os
 
 class AuthConfig(BaseSettings):
     class Config:
-        env_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
+        env_file = ".env"
         env_file_encoding = "utf-8"
         env_prefix = "JWT_"
         extra = "ignore"
